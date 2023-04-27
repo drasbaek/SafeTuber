90e1dedebaff49b996d39286f777286912a92cb2
Anton Drasbæk <202008161@post.au.dk>
2023-04-27 19:05:49 +0200

    working version

--- src/mp4-to-text.py
+++ src/mp4-to-text.py
@@ -1,11 +1,34 @@
-from transformers import WhisperProcessor, WhisperForConditionalGeneration
 from transformers import pipeline
-from scipy.io import wavfile
+import torch
 
-samplerate, audio = wavfile.read('./out/The BEST of CREW MEMES!.wav')
+def initialize_transcriber():
+    # define device to use
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-transcriber = pipeline('automatic-speech-recognition', model='openai/whisper-tiny.en')
+    # initialize transcriber
+    transcriber = pipeline('automatic-speech-recognition', 
+                           model='openai/whisper-tiny.en',
+                           chunk_length_s = 30, # must be 30 to chunk correctly
+                           device = device)
+    return transcriber
 
-test = transcriber('audio.wav')
+def transcribe_audio(filename, transcriber):
+    file_path = "./out/" + filename
 
-print(test)
+    # get audio
+    transcript_dict = transcriber(file_path)
+
+    # save dict to file
+    text = transcript_dict['text']
+
+    return text
+
+
+def main():
+    transcriber = initialize_transcriber()
+    filename = "Robert ＂Kool＂ Bell on Kool & The Gang Being the Most-Sampled Group, Sampled 1,800 Times (Part 1).wav"
+    text = transcribe_audio(filename, transcriber)
+    print(text)
+
+if __name__ == "__main__":
+    main()
