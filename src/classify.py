ebfc8dc05264086ce2a7c58ac87ffc67c74494a4
Anton Drasbæk <202008161@post.au.dk>
2023-05-02 22:50:16 +0200

    updated data path

--- src/classify.py
+++ src/classify.py
@@ -3,13 +3,13 @@ import pandas as pd
 import tqdm
 
 # load data with transcripts
-data = pd.read_csv("data/top-youtubers-with-text.csv")
+data = pd.read_csv("data/top-youtubers-curated.csv")
 
 # change text chunks to list
 data["text_chunks"] = data["text_chunks"].apply(eval)
 
 # load toxicity classifier
-classifier = pipeline("text-classification", model = "martin-ha/toxic-comment-model")
+classifier = pipeline("text-classification", model = "unitary/toxic-bert")
 
 def classify_text_chunks(text_chunks, classifier):
     """
@@ -23,22 +23,26 @@ def classify_text_chunks(text_chunks, classifier):
         # classify text chunk
         classification = classifier(text_chunk)[0]
 
+        print(classification)
+
         # append classification label to list
         classifications.append(classification["label"])
-    
+
+    return classifications
+
+def toxicity_aggregates(text_chunks, classifications):
+    """
+    Calculate toxicity aggregates.
+    """
     # calculate percentage of toxic comments
     n_toxic = classifications.count("toxic")
     n_comments = len(classifications)
     pct_toxic = n_toxic / n_comments
 
-    # identify if there are any toxic comment
-    if "toxic" in classifications:
-        toxic = True
-    else:
-        toxic = False
+    # get a toxic comment
+    toxic_comment = text_chunks[classifications.index("toxic")]
 
-
-    return classifications, pct_toxic, toxic
+    return pct_toxic, toxic_comment
 
 # apply function to all text chunks in dataframe
 for index, row in data.iterrows():
@@ -46,11 +50,14 @@ for index, row in data.iterrows():
     text_chunks = row["text_chunks"][0]
 
     # classify text chunks
-    classifications, pct_toxic, toxic = classify_text_chunks(text_chunks, classifier)
+    classifications = classify_text_chunks(text_chunks, classifier)
 
-    # save pct toxic and toxic to dataframe
+    # calculate toxicity aggregates
+    pct_toxic, toxic_comment = toxicity_aggregates(text_chunks, classifications)
+
+    # save to dataframe
+    data.at[index, "example_toxic_comment"] = toxic_comment
     data.at[index, "pct_toxic"] = pct_toxic
-    data.at[index, "any_toxic"] = toxic
 
 # write to csv
 data.to_csv("data/top-youtubers-with-text-and-classifications.csv", index = False)
