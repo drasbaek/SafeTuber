67e5404b3d28af40a3f735c5be22dcbee7e18974
Anton Drasbæk <202008161@post.au.dk>
2023-05-02 22:50:41 +0200

    finished functions

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
-import pandas
+import pandas as pd
 from mp3_to_text import *
 from yt_to_mp3 import *
 import tqdm
+import random
 
 path = Path(__file__)
 audio_path = path.parents[1] / "audio_files"
 
 # load in the data
-data = pandas.read_csv("data/top-youtubers.csv")
+data = pd.read_csv("data/top-youtubers.csv")
 
 # only keep first 100 rows
 data = data.iloc[:2]
+
+# create empty column for text chunks that is object type
+data["text_chunks"] = ""
 
 print(data)
 
@@ -28,18 +32,30 @@ for index, row in tqdm.tqdm(data.iterrows(), total = data.shape[0]):
     # download videos
     download_channel(n_vids = 2, video_urls = video_urls, outpath = audio_path)
 
+    # define empty list to store text chunks
+    all_text_chunks = []
+
     for audio in os.listdir(audio_path):
         text_chunks = transcribe_audio(audio, transcriber)
         text_chunks_cln = clean_text(text_chunks)
+        all_text_chunks.append(text_chunks_cln)
 
-        # save to file in out folder
-        with open("out/" + audio + ".txt", "w") as f:
-            for chunk in text_chunks_cln:
-                f.write(chunk + "\n")
+        # delete audio file
+        os.remove(audio_path / audio)
     
     # delete audio files
     for audio in os.listdir(audio_path):
         os.remove(audio_path / audio)
+
+    # shuffle text chunks
+    all_text_chunks = random.shuffle(all_text_chunks)
+
+    # save text chunks to dataframe
+    data.at[index, "text_chunks"] = all_text_chunks
+
+# write to a new csv
+data.to_csv("data/top-youtubers-with-text.csv", index = False)
+
 
 
 
