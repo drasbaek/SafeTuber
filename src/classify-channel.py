4cee861fba14456d286ee255c46067219f02fca3
Anton Drasbæk <202008161@post.au.dk>
2023-05-03 16:38:35 +0200

    renamed top folder

---
+++ src/classify-channel.py
@@ -0,0 +1,269 @@
+# load packages
+from pytube import Channel
+from yt_dlp import YoutubeDL
+from pathlib import Path
+from tqdm import tqdm
+from transformers import pipeline
+import torch
+from utils import *
+import pandas as pd
+import os
+import random
+import io
+import sys
+
+def define_paths():
+    # define path
+    path = Path(__file__)
+
+    # define inpath
+    inpath = path.parents[1] / "data"
+
+    # define outpath
+    outpath = path.parents[1] / "out"
+
+    # create dir for audio files if it doesn't exist
+    if not os.path.exists(path.parents[1] / "audio_files"):
+        os.makedirs(path.parents[1] / "audio_files")
+    
+    # define path to temporary audio storage
+    audio_path = path.parents[1] / "audio_files"
+
+    return inpath, outpath, audio_path
+
+
+def initialize_models():
+    # define device to use
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+    # initialize transcriber
+    transcriber = pipeline('automatic-speech-recognition', 
+                           model='openai/whisper-base.en',
+                           chunk_length_s = 30, # must be 30 to chunk correctly
+                           return_timestamps=True,
+                           device = device)
+    # initialize classifier
+    classifier = pipeline("text-classification", 
+                          model = "martin-ha/toxic-comment-model")
+
+    return transcriber, classifier
+
+def get_channel_vids(channel_url):
+    """
+    Uses yt_dlp to get the video urls from a channel url.
+    It is necessary to obtain these urls from the terminal output as extract_info does not return those
+    """
+    
+    # Create a StringIO object to capture stdout
+    captured_output = io.StringIO()
+
+    # Redirect stdout to the StringIO object
+    sys.stdout = captured_output
+
+    # Run your code
+    ydl_opts = {'outtmpl': '%(id)s.%(ext)s', 'playlistend': 10}
+    with YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(
+            'https://www.youtube.com/channel/UCX6OQ3DkcsbYNE6H8uQQuVA',
+            download=False
+        )
+
+    # Restore stdout to its original value
+    sys.stdout = sys.__stdout__
+
+    # Print the captured output
+    output = captured_output.getvalue()
+
+    # extract all urls from output
+    urls = []
+
+    for line in output.splitlines():
+        if "https://www.youtube.com/watch?v=" in line:
+            # extract only the url from the line
+            url = line.split(" ")[-1]
+            urls.append(url)
+
+    return urls
+
+def download_mp4(outpath, url, max_duration, min_duration):
+    # get info
+    ydl = YoutubeDL()
+    info_dict = ydl.extract_info(url, download=False)
+
+    # check duration
+    duration = info_dict.get('duration')
+
+    # if duration is too long, skip
+    if duration > 1200:
+        print("Video too long, skipping to next..." + url)
+        return 0 # return 0 for fail
+    
+    if duration < 120:
+        print("Video too short, skipping to next..." + url)
+        return 0 # return 0 for fail
+
+    # else download
+    else:
+        ydl_opts = {
+        'outtmpl': str(outpath) + '/%(title)s.%(ext)s',
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+    }],
+        }
+
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            return 1 # return 1 for success
+
+def download_channel(n_vids, video_urls, outpath):
+    print("Downloading videos...")
+    # get max n_vids from channel
+    max_attempts = len(video_urls)
+
+    # set download and attempt counters
+    n_downloads = 0
+    n_attempt = 0  
+
+    # download videos
+    while n_downloads < n_vids and n_attempt < max_attempts:
+        url = video_urls[n_attempt]
+        n_attempt += 1
+    
+        try:
+            success_fail = download_mp4(outpath, url, max_duration=720, min_duration=60)
+            n_downloads += success_fail
+        
+        except:
+            print("Error downloading video: ", url)
+
+def transcribe_audio(filename, transcriber, audio_path):
+    print("Transcribing audio...")
+    file_path = str(audio_path / filename)
+
+    # get audio
+    transcript_dict = transcriber(file_path)
+
+    # save dict to file
+    text_chunks = transcript_dict['chunks']
+
+    # remove timestamps
+    text_chunks = [item["text"] for item in text_chunks]
+
+    print(text_chunks)
+
+    return text_chunks
+
+def classify_transcript(text_chunks, classifier):
+    print("Classifying text chunks...")
+    """
+    Classify text chunks as toxic or not toxic.
+    """
+    # initialize empty list to store classifications
+    classifications = []
+
+    # loop over text chunks with tqdm
+    for text_chunk in text_chunks:
+        # classify text chunk
+        classification = classifier(text_chunk)[0]
+
+        # append classification label to list
+        classifications.append(classification["label"])
+
+    return classifications
+
+def toxicity_aggregates(text_chunks, classifications):
+    """
+    Calculate toxicity aggregates.
+    """
+    # calculate percentage of toxic comments
+    n_toxic = classifications.count("toxic")
+    n_comments = len(classifications)
+
+    # get a toxic comment if there is one
+    if n_toxic != 0:
+        pct_toxic = float(n_toxic) / float(n_comments)
+        # get all toxic comments
+        toxic_comments = [text_chunks[i] for i in range(len(text_chunks)) if classifications[i] == "toxic"]
+    else:
+        toxic_comments = None
+        pct_toxic = 0
+
+    return pct_toxic, toxic_comments
+
+def main():
+    # define paths
+    inpath, outpath, audio_path = define_paths()
+
+    # load data from inpath
+    data = pd.read_csv(inpath / "top-youtubers.csv")
+
+    # only keep two random rows (temp)
+    data = data.sample(n = 5)
+
+    # create empty columns for later variables
+    data["transcript_chunks"] = None
+    data["pct_toxic"] = None
+    data["toxic_comments"] = None
+
+    # initialize models
+    transcriber, classifier = initialize_models()
+
+    # loop through all channels (rows)
+    for i, row in data.iterrows():
+        # get channel url
+        channel_url = row["ha-link"]
+
+        # get channel videos
+        video_urls = get_channel_vids(channel_url)
+
+        # download videos
+        download_channel(n_vids = 2, video_urls = video_urls, outpath = audio_path)
+
+        # define empty list to store text chunks
+        all_text_chunks = []
+
+        # loop through all files in audio_path
+        for audio_file in os.listdir(audio_path):
+            # transcribe audio
+            text_chunks = transcribe_audio(audio_file, transcriber, audio_path)
+
+            # clean transcript
+            text_chunks_cln = clean_text(text_chunks)
+
+            # append all elements in text_chunks_cln to all_text_chunks
+            all_text_chunks.extend(text_chunks_cln)
+
+            # delete audio file
+            os.remove(audio_path / audio_file)
+        
+        # shuffle list
+        random.shuffle(all_text_chunks)
+
+        # classify text chunks
+        classifications = classify_transcript(all_text_chunks, classifier)
+
+        # calculate toxicity aggregates
+        pct_toxic, toxic_comments = toxicity_aggregates(all_text_chunks, classifications)
+
+        # append to dataframe
+        data.at[i, "transcript_chunks"] = all_text_chunks
+        data.at[i, "toxic_comments"] = toxic_comments
+        data.loc[i, "pct_toxic"] = pct_toxic
+
+    # save dataframe to outpath
+    data.to_csv(outpath / "top-youtubers-with-classifications.csv")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
---
+++ src/utils.py
@@ -0,0 +1,81 @@
+# Desc: Utility functions for the project
+'''
+'''
+def remove_long_chunks(text_chunks):
+    '''
+    Removes long chunks as most of these will be due to an error where a word is repeated often in the audio transcription
+    '''
+    # initialize new list
+    new_text_chunks = []
+
+    # loop through chunks
+    for text in text_chunks:
+
+        # if text is too long, skip
+        if len(text.split()) > 25:
+            continue
+
+        # else add to new list
+        else:
+            new_text_chunks.append(text)
+
+    return new_text_chunks
+
+
+def remove_duplicates(text_chunks):
+    # initialize new list
+    new_text_chunks = []
+
+    # initialize previous text
+    prev_text = ""
+
+    # loop through chunks
+    for text in text_chunks:
+
+        # if text is the same as previous text, skip
+        if text == prev_text:
+            continue
+
+        # else add to new list
+        else:
+            new_text_chunks.append(text)
+            prev_text = text
+
+    return new_text_chunks
+
+def concatenate_chunks(text_chunks):
+    joined_chunks = []
+    current_string = ""
+
+    for i, string in enumerate(text_chunks):
+        
+        # Join current string with next string if current string is too short or it doesn't end in a period
+        if len(current_string.split()) < 10:
+            current_string += " " + string
+
+        elif current_string[-1] != ".":
+            current_string += " " + string
+        
+        # Add current string to joined data if it's long enough
+        else:
+            joined_chunks.append(current_string.strip())
+            current_string = string
+
+        # Add the last string to the joined data
+        if i == len(text_chunks) - 1:
+            joined_chunks.append(current_string.strip())
+    
+    return joined_chunks
+
+
+def clean_text(text_chunks):
+    # remove long chunks
+    text_chunks = remove_long_chunks(text_chunks)
+
+    # remove duplicates
+    text_chunk_no_dup = remove_duplicates(text_chunks)
+
+    # concatenate chunks
+    text_chunks_concat = concatenate_chunks(text_chunk_no_dup)
+
+    return text_chunks_concat
