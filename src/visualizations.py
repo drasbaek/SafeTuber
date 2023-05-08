3a93d54c1dbf1cb5cd1001ad3e3ee70de71ae002
Anton Drasbæk <202008161@post.au.dk>
2023-05-08 09:18:37 +0200

    added two new plots

--- src/visualizations.py
+++ src/visualizations.py
@@ -51,6 +51,67 @@ def plot_share_of_toxic(data, results_path):
     # save plot
     plt.savefig(results_path / "share-of-toxic-channels.png")
 
+def plot_toxicity_by_category(data, results_path):
+    '''
+    Plots a bar chart displaying the share of toxic comments by category.
+    Plot is saved as a png file in the out folder.
+
+    Args:
+        data (pd.DataFrame): dataframe with the results of the classifier
+    '''
+    # get share of toxic comments by category
+    share_toxic = data.groupby("categories")["pct_toxic"].mean().reset_index()
+
+    # order by share of toxic comments
+    share_toxic = share_toxic.sort_values(by="pct_toxic", ascending=False)
+
+    # set the color palette
+    #colors = ['#FF6E78', '#50C878']
+    #sns.set_palette(sns.color_palette(colors))
+
+    # create bar chart
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # plot bar chart
+    sns.barplot(x="categories", y="pct_toxic", data=share_toxic, ax=ax)
+
+    # set title
+    ax.set_title("Share of toxic comments by channel category")
+
+    # space out x-axis labels
+    plt.xticks(rotation=45, ha="right")
+
+    # save plot
+    plt.savefig(results_path / "toxicity-by-category.png")
+
+def plot_most_toxic_channels(data, results_path):
+    '''
+    Plots a barchart showing the 10 most toxic channels in the top 100 as well as the average for all channels.
+    Plot is saved as a png file in the out folder.
+    '''
+    # get the 10 most toxic channels
+    most_toxic = data.sort_values(by="pct_toxic", ascending=False).head(10)
+
+    # get the average toxicity for all channels
+    avg_toxic = data["pct_toxic"].mean()
+
+    # add average to dataframe
+    most_toxic = most_toxic.append({"name": "Top 100 Average", "pct_toxic": avg_toxic}, ignore_index=True)
+
+    # create bar chart
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # plot bar chart
+    sns.barplot(x="name", y="pct_toxic", data=most_toxic, ax=ax)
+
+    # set title
+    ax.set_title("The most toxic channels in the top 100")
+
+    # space out x-axis labels
+    plt.xticks(rotation=45, ha="right")
+
+    # save plot
+    plt.savefig(results_path / "most-toxic-channels.png")
 
 
 def main():
@@ -60,10 +121,14 @@ def main():
     # load data
     data = pd.read_csv(results_path / "top-youtubers-classified.csv")
 
-    print(data.head())
-
     # plot share of toxic channels
     plot_share_of_toxic(data, results_path)
+
+    # plot toxicity by category
+    plot_toxicity_by_category(data, results_path)
+
+    # plot most toxic channels
+    plot_most_toxic_channels(data, results_path)
 
 
 if __name__ == "__main__":
